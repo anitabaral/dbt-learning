1d0f0bb221f3b3fe4e0de50c3fe356b4ac891f0f
anitabaral <baralanita2@gmail.com>
2022-03-10 12:05:57 +0545

    Adding the sql query

--- dbt_learning/models/example/fastfoods.sql
+++ dbt_learning/models/example/fastfoods.sql
@@ -1,4 +1,9 @@
-SELECT DISTINCT(province) AS code, province_name AS province FROM dev_pizzastore.datafiniti 
+WITH CTE_province_data AS
+(SELECT DISTINCT(dev_pizzastore.province_data.city) AS province_name, province AS province_code 
+FROM dev_pizzastore.datafiniti 
 INNER JOIN dev_pizzastore.province_data
-ON dev_pizzastore.datafiniti.province = dev_pizzastore.province_data.province_code
-ORDER BY code ASC
+ON dev_pizzastore.datafiniti.province = dev_pizzastore.province_data.state_code
+ORDER BY province_name
+)
+
+SELECT * FROM CTE_province_data
